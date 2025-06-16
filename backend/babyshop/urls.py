afb4bc595de08b007e1d7ec495004be02de500e3
alia jonathan <aliajonathan1@gmail.com>
2025-06-16 09:43:15 +0300

    building api endpoints

--- backend/babyshop/urls.py
+++ backend/babyshop/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from .views import root_view
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('', root_view, name='root'),
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
